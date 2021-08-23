12cb5443b46c5dbfe9445314932ab3032eec9057
Adam Li <adam2392@gmail.com>
2021-08-23 14:16:11 -0400

    Update mne_connectivity/conftest.py

--- mne_connectivity/conftest.py
+++ mne_connectivity/conftest.py
@@ -9,7 +9,6 @@ import pytest
 @pytest.fixture(autouse=True)
 def close_all():
     """Close all matplotlib plots, regardless of test status."""
-    # This adds < 1 µS in local testing, and we have ~2500 tests, so ~2 ms max
     import matplotlib.pyplot as plt
     yield
     plt.close('all')
